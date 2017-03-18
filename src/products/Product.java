af231deda182a0b5477ef3a8ee9775e64ea26650
jevgenieva <jevgenieva@gmail.com>
2017-03-18 11:14:18 +0200

    tweaked Product class

--- src/products/Product.java
+++ src/products/Product.java
@@ -1,19 +1,33 @@
 package products;
 
-public abstract class Product {
-	//add fields and constructors
+public class Product {
+	
 	private int callories;
+	private String type;
+	private String name;
 
-	//this is the abstract mother-class of all our products
-	protected Product(int callories) throws InvalidProductParamException {
+	//insert column "type" into db
+	//users can't create new products
+	
+	protected Product(int callories, String type, String name) throws InvalidProductParamException {
 		if(callories > 0)
 			this.callories = callories;
 		else
 			throw new InvalidProductParamException();
+		
+		if(type != null && !type.isEmpty()) 
+			this.type = type;
+		else
+			throw new InvalidProductParamException();
+		
+		if(name != null && !name.isEmpty()) 
+			this.name = name;
+		else
+			throw new InvalidProductParamException();	
 	}
 	
 	public static class InvalidProductParamException extends Exception{
-		//print a message on the web page
+		//print a msg into the console or log file
 	}
 }
 
